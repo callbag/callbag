06eb362d144a6024cff9e9fef2e9fedcf2c4b166
Andre Staltz <andre@staltz.com>
2018-01-29 13:50:20 +0200

    Update typescript types

--- types.d.ts
+++ types.d.ts
@@ -9,6 +9,6 @@ export type RESERVED_7 = 7;
 export type RESERVED_8 = 8;
 export type RESERVED_9 = 9;
 
-export type Callbag = (type: number, data: any) => void;
+export type Callbag = (type: START | DATA | END, payload?: any) => void;
 export type Factory = (...args: Array<any>) => Callbag;
 export type Operator = (...args: Array<any>) => (source: Callbag) => Callbag;
